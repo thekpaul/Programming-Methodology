ecade105dfa37d67feba9af43b076f8e6a59fdb7
thekpaul <44695374+thekpaul@users.noreply.github.com>
2019-04-26 00:20:33 +0900

    Rename file to avoid confusion

---
+++ Cpp/03_iosheader.hpp
@@ -0,0 +1,11 @@
+#ifndef _IOSHEADER_H_ // Check if header is yet undefined
+#define _IOSHEADER_H_ // If checked, define header
+
+#include <iostream>
+
+class classy {
+public:
+    void std::cout << "Hello World!" << std::endl;
+};
+
+#endif // End selection control
