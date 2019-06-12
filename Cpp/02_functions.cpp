d14e02b0d55df1c3bbc1e64025ea32cdb4a57151
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2019-06-12 13:36:57 +0900

    Start Commit on II.2

--- Cpp/02_functions.cpp
+++ Cpp/02_functions.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
-using namespace std;
+
+int SumofThree(int a, int b, int c) {
+    return a + b + c; // Returns the sum of three integers, an integer
+}
+
+void SwapTwo(int &a, int &b) {
+    int temp = a;
+    a = b; b = temp;
+    // No return values
+}
 
 int main(void) {
-    <++>;
+    SumofThree(3, 4, 5); // Result: 12
+    int a = 3, b = 4;
+    SwapTwo(a, b); // No return values, therefore no result
+    std::cout << a << " " << b << std::endl; // Result: 4 3
     return 0;
 }
