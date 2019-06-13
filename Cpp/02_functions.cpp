6d29d4bfbd9d791154d9e45b4094dcc13cdd483d
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2019-06-13 12:18:15 +0900

    Finish Commit on II.2

--- Cpp/02_functions.cpp
+++ Cpp/02_functions.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int SumofThree(int a, int b, int c) {
-    return a + b + c; // Returns the sum of three integers, an integer
+    return a + b + c;
+    // Returns the sum of three integers
 }
 
 void SwapTwo(int &a, int &b) {
@@ -10,10 +11,59 @@ void SwapTwo(int &a, int &b) {
     // No return values
 }
 
-int main(void) {
-    SumofThree(3, 4, 5); // Result: 12
+int SumofFour(int a = 1, int b = 2, int c = 3, int d = 4) {
+    return a + b + c + d;
+    // Returns the sum of four integers
+}
+
+void SwapByValue(int a, int b) {
+    int temp = a; // Save VALUE OF a
+    a = b; b = temp; // Switch values
+}
+void SwapByReference(int& a, int& b) {
+    int temp = a; // Save VALUE OF a
+    a = b; b = temp; // Switch values
+}
+void SwapByPointer(int* a, int* b) {
+    int temp = *a; // Save value at ADDRESS a
+    *a = *b; *b = temp;
+    // Switch address in pointer
+}
+
+inline int ProductThree(int a, int b, int c) {
+    return a * b * c;
+    // Returns the product of three integers.
+}
+
+int Factorial(int a) {
+    if (a <= 0) return 1;
+    // Return a terminating value
+    else return a * Factorial(a - 1);
+    // Provide decremental value for rescursion
+}
+
+int main(int argc, char* argv[]) {
+    std::cout << SumofThree(3, 4, 5); // Result: 12
+    std::cout << std::endl;
     int a = 3, b = 4;
     SwapTwo(a, b); // No return values, therefore no result
-    std::cout << a << " " << b << std::endl; // Result: 4 3
+    std::cout << a << " " << b; // Result: 4 3
+    std::cout << std::endl;
+    std::cout << SumofFour(2, 3); // Result: 12
+    std::cout << std::endl;
+    int x = 5, y = 3;
+    SwapByValue(x, y); // Provide values
+    std::cout << x << " " << y << " ";
+    // Result: 5 3 -> No change
+    SwapByReference(x, y); // Provide variables
+    std::cout << x << " " << y << " ";
+    // Result: 3 5 -> Swap Variables
+    SwapByPointer(&x, &y); // Provide addresses
+    std::cout << x << " " << y << " ";
+    // Result: 5 3 -> Swap Addresses
+    std::cout << std::endl;
+    std::cout << ProductThree(3, 4, 5); // Result: 60
+    std::cout << std::endl;
+    std::cout << Factorial(5); // Result: 60
     return 0;
 }
